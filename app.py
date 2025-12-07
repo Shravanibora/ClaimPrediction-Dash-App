b885a52debf14f996444e16f085a6a576189415b
Shravanibora <shravanibora40@gmail.com>
2025-12-07 00:54:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ payer_transitions_url = "https://drive.google.com/uc?export=download&id=1iatTcuu
 
 # Load all main tables from Drive
 patients = pd.read_csv(patients_url, usecols=["Id", "BIRTHDATE", "GENDER"])
-encounters = pd.read_csv(encounters_url, usecols=["PATIENT", "DENIAL_REASON", "ENCOUNTERCLASS", "REASONDESCRIPTION", "PAYER", "TOTAL_CLAIM_COST"])
+encounters = pd.read_csv(encounters_url, usecols=["PATIENT", "denial_reason", "ENCOUNTERCLASS", "REASONDESCRIPTION", "PAYER", "TOTAL_CLAIM_COST"])
 claims = pd.read_csv(claims_url, usecols=["PATIENTID", "STATUS1", "APPOINTMENTID", "STATUS2", "STATUSP"])
 claims_transactions = pd.read_csv(claims_transactions_url, usecols=["CLAIMID", "TODATE", "AMOUNT"])
 payer_transitions = pd.read_csv(payer_transitions_url, usecols=["PATIENT", "PAYER"])
@@ -1354,6 +1354,7 @@ def update_output(n_clicks, username, password):
 # =========================================================
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 
